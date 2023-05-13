0875725660462bb2a628ec6704499816a228a2b6
calumrowan <63161217+calumrowan@users.noreply.github.com>
2023-05-13 19:05:38 +0100

    Update Share.tsx
    Bugfix

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -55,7 +55,11 @@ export function Share({
       })
       .join("\n");
 
-    return [title, guessString, "https://tradle-destinations.onrender.com/"].join("\n");
+    return [
+      title,
+      guessString,
+      "https://tradle-destinations.onrender.com",
+    ].join("\n");
   }, [dayString, guesses, hideImageMode, rotationMode, theme, isAprilFools]);
 
   return (
